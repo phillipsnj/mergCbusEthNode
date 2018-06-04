fe0f29d129337f4e7185d4d765fc5d2ea48945b2
phillipsnj <nigeljphillips@yahoo.co.uk>
2018-06-04 18:26:55 +0100

    Initial Version

---
+++ testMergEthNode.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const pcCbus = require('mergEthNode')
+//const pcCbus = require('../mergEthNode/mergEthNode')
+
+const file = './nodeConfig.json'
+
+const NET_PORT = 5550;
+const NET_ADDRESS = "mergDev0";
+
+let node = new pcCbus.mergEthNode(file,NET_ADDRESS,NET_PORT);
+
+console.log('New Node :'+node.nodeId)
+console.log('New Name :'+node.name)
+//console.log('Action 2 :'+node.actions['90'])
+//console.log(`New Actions :${JSON.stringify(node.actions)}`)
+
+const argv = require('minimist')(process.argv.slice(2))
+
+node.on('event', function (task) {
+    console.log(`Event :${JSON.stringify(task)}`)
+})
+
+if (argv.setup) {
+    console.log(`Set TEACH_MODE = True`)
+    //node.setTeachModeTrue()
+    node.TEACH_MODE = true
+    node.cbusSend(node.RQNN())
+} else {
+    node.cbusSend(node.ASON(1))
+}
+
+
